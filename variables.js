cdcc2d9af82bf3c1010ac49390c696284ef17640
Devesh Kumar <118952288+xcoder45@users.noreply.github.com>
2024-02-14 16:28:27 +0530

    commit

---
+++ variables.js
@@ -0,0 +1,22 @@
+const accountId = 11
+let accountEmail = "xcoderdevesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 899999
+
+accountEmail ="devesg@gmail.com"
+accountPassword ="2344"
+accountCity="goa"
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+/*
+ 
+   do not use var variables because of issue in block scope and functional scope .
+
+   use const and let . hehehehehe
+
+
+*/
+
