ae82478ea2d441adfdb41dc95d1ccc169adcdd37
Devesh Kumar <118952288+xcoder45@users.noreply.github.com>
2024-02-14 17:11:36 +0530

    problem of codechef

---
+++ problem.js
@@ -0,0 +1,24 @@
+// Chef wants to become fit for which he decided to walk to the office and return home by walking. It is known that Chef's office is 
+// �
+// X km away from his home.
+
+// If his office is open on 
+// 5
+// 5 days in a week, find the number of kilometers Chef travels through office trips in a week.
+
+// Input Format
+// First line will contain 
+// �
+
+// Each test case contains of a single line consisting of single integer 
+// �
+// X.
+// Output Format
+// For each test case, output the number of kilometers Chef travels through office trips in a week.
+
+
+
+// */
+
+let x = 10
+console.log(x*10)
