3807e5a1248ada3d0cf33f84f2ecd40c5d2d2cd8
Devesh Kumar <118952288+xcoder45@users.noreply.github.com>
2024-02-14 16:50:07 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+"use strict"   // treat all js code as newer versions
+
+// alert("hello") // we are using node js not browser
+
+console.log(3+3) 
+
+console.log("devesh")
+
+
+/*
+some taking notes on data types
+
+number => 2 to power 53
+bigint 
+string ="we can use this "
+or 'we can use this also'
+
+boolean => true/false
+
+null => standalone value (type =object)
+
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = uniquness
+
+
+// object
+
+
+
+
+*/
+
+console.log(typeof undefined)
